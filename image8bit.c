8c063f3781b352333f3b0e2fbd5b9c936e0f6514
RuiAlbuquerque-15 <149696830+RuiAlbuquerque-15@users.noreply.github.com>
2023-11-09 15:59:45 +0000

    Update image8bit.c
    foram feitas as funções G e ImageStats

--- image8bit.c
+++ image8bit.c
@@ -322,6 +322,19 @@ int ImageMaxval(Image img) { ///
 void ImageStats(Image img, uint8* min, uint8* max) { ///
   assert (img != NULL);
   // Insert your code here!
+  int x, y;
+
+  for (x = 0; x < img->width; x++) {
+	for (y = 0; y < img->height; y++) {
+	  uint8 pixelColor = ImageGetPixel(img, x, y);
+	  if (*min > pixelColor) {
+		*min = pixelColor;
+	  }
+	  else if (*max < pixelColor) {
+		*max = pixelColor;
+	  }
+	}
+  }
 }
 
 /// Check if pixel position (x,y) is inside img.
@@ -349,6 +362,8 @@ int ImageValidRect(Image img, int x, int y, int w, int h) { ///
 static inline int G(Image img, int x, int y) {
   int index;
   // Insert your code here!
+  index = x + (y * img->width);
+  index += img->pixel;
   assert (0 <= index && index < img->width*img->height);
   return index;
 }
