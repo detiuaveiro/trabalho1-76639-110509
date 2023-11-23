43e568c7656274ca83f602d4208f3d03f0e5b046
RuiAlbuquerque-15 <149696830+RuiAlbuquerque-15@users.noreply.github.com>
2023-11-23 14:56:53 +0000

    Update image8bit.c
    ImageBlur Fixes

--- image8bit.c
+++ image8bit.c
@@ -682,7 +682,7 @@ int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) { ///
 void ImageBlur(Image img, int dx, int dy) { ///
   // Insert your code here!
 
-  Image imgOriginal = ImageCreate(img->width, img->height, img->maxval);
+  Image imgOriginal = img;
   
   for (int i = 0; i < img->width; i++) {
 	  for (int j = 0; j < img->height; j++) {
